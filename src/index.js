afa28bdf94073e79ff84a2d2d2d1bafa68d45ae1
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2023-08-13 09:54:24 +0300

    ADD: webpack config file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { AlertService } from "./app/alert.service";
+import { run } from "./app/app";
+import { ComponentService } from "./app/component.service";
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["css-loader", "style-loader"],
+      },
+    ],
+  },
+};
